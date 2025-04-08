289224a9f8d2be712d92fbd74c65979e6a16ca77
Dean Baberowski <deanbaberows@gmail.com>
2025-04-08 11:54:59 +1000

    Added CSS and JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    alert("Thank You I'll get back to you in 1-2 business days");
+})
