de25dabd5d2e911d5c2a8930077b730f28365a7c
Dean Baberowski <deanbaberows@gmail.com>
2025-04-10 09:24:59 +1000

    Updated CSS and JavaScript and added images for GitHub

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ document.querySelector("body").appendChild(h2);
 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', () => {
     alert("Thank You I'll get back to you in 1-2 business days");
 })
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu-button');
+    const navbar = document.getElementById('navbar');
+  
+    menuButton.addEventListener('click', function() {
+      const expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+      menuButton.setAttribute('aria-expanded', !expanded);
+      navbar.classList.toggle('open');
+    });
+  });
+  
